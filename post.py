2fd2cb923101942d41cc87e3013986222525234a
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-09 16:10:30 +0200

    Unittesting

--- post.py
+++ post.py
@@ -1,8 +1,12 @@
 class Post:
-    def __init__(self, title, content, author):
+    def __init__(self, title, content, author, new):
         self.title = title
         self.content = content
         self.author = author
+        self.new = 'new'
+
+    def __repr__(self):
+        return f'{self.title}, {self.author}, {self.content}'
 
     def json(self):
         return {
--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -4,11 +4,17 @@ from post import Post
 
 class PostTest(TestCase): # Create a new class that inherits from TestCase
     def test_create_post(self): # Test that we can create a post with a title and content
-        p = Post('Test', 'Test Content')  # Create a new post object
-        self.assertEqual('Test', p.title) # Assert that the title of the post object is 'Test'
-        self.assertEqual('Test Content', p.content) # Assert that the content of the post object is 'Test Content'
+        p = Post('Test', 'Test Content', 'Author', 'new')  # Create a new post object
+        self.assertEqual('Test', p.title)  # Assert that the title of the post object is 'Test'
+        self.assertEqual('Test Content', p.content)  # Assert that the content of the post object is 'Test Content'
+        self.assertEqual('Author', p.author)  # Assert that the author of the post object is 'Author'
+        self.assertEqual('new', p.new)  # Assert that the new of the post object is 'new'
 
-    # def test_json(self): # Test that the json method of the post object works as expected
-    #     p = Post('Test', 'Test Content') # Create a new post object
-    #     expected = {'title': 'Test', 'content': 'Test Content'} # Define what the expected result of calling .json() on the post object should be
-    #     self.assertDictEqual(expected, p.json()) # Assert that the result of calling .json() on the post object is equal to the expected result
+    def test_json(self): # Test that the json method of the post object works as expected
+        p = Post('Test', 'Test Content', 'Author', 'new')  # Create a new post object
+        expected = {'title': 'Test', 'content': 'Test Content', 'author': 'Author', 'new': 'new'}
+        self.assertDictEqual(expected, p.json())
+
+    def test_repr(self):
+        p = Post('Test', 'Test Content', 'Author', 'new')
+        self.assertEqual(str(p), 'Test, Author, Test Content')
