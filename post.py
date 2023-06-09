0dd3c5813ea43e3eeb1ff2cc04d64cac7d5c4a5e
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-09 15:58:56 +0200

    Unittesting

--- post.py
+++ post.py
@@ -1,4 +1,13 @@
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, title, content, author):
         self.title = title
         self.content = content
+        self.author = author
+
+    def json(self):
+        return {
+            'title': self.title,
+            'content': self.content,
+            'author': self.author,
+            'new': 'new'
+        }
--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -7,3 +7,8 @@ class PostTest(TestCase): # Create a new class that inherits from TestCase
         p = Post('Test', 'Test Content')  # Create a new post object
         self.assertEqual('Test', p.title) # Assert that the title of the post object is 'Test'
         self.assertEqual('Test Content', p.content) # Assert that the content of the post object is 'Test Content'
+
+    # def test_json(self): # Test that the json method of the post object works as expected
+    #     p = Post('Test', 'Test Content') # Create a new post object
+    #     expected = {'title': 'Test', 'content': 'Test Content'} # Define what the expected result of calling .json() on the post object should be
+    #     self.assertDictEqual(expected, p.json()) # Assert that the result of calling .json() on the post object is equal to the expected result
