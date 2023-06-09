db428d10a0e20c5ed2de1dbc0e3ad637e3016a14
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-09 15:52:22 +0200

    Unittesting

--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -1,7 +1,9 @@
-from unittest import Testcase
+from unittest import TestCase
 from post import Post
 
 
-class PostTest(Testcase):
-    def test_create_post(self):
-        pass
+class PostTest(TestCase): # Create a new class that inherits from TestCase
+    def test_create_post(self): # Test that we can create a post with a title and content
+        p = Post('Test', 'Test Content')  # Create a new post object
+        self.assertEqual('Test', p.title) # Assert that the title of the post object is 'Test'
+        self.assertEqual('Test Content', p.content) # Assert that the content of the post object is 'Test Content'
