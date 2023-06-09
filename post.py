104f749dfe2f0cd434b8e0f97c8b0338efb2039e
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-09 20:30:31 +0200

    Unittesting

--- post.py
+++ post.py
@@ -1,9 +1,8 @@
 class Post:
-    def __init__(self, title, content, author, new):
+    def __init__(self, title, content, author):
         self.title = title
         self.content = content
         self.author = author
-        self.new = 'new'
 
     def __repr__(self):
         return f'{self.title}, {self.author}, {self.content}'
@@ -13,5 +12,4 @@ class Post:
             'title': self.title,
             'content': self.content,
             'author': self.author,
-            'new': 'new'
         }
--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -8,13 +8,14 @@ class PostTest(TestCase): # Create a new class that inherits from TestCase
         self.assertEqual('Test', p.title)  # Assert that the title of the post object is 'Test'
         self.assertEqual('Test Content', p.content)  # Assert that the content of the post object is 'Test Content'
         self.assertEqual('Author', p.author)  # Assert that the author of the post object is 'Author'
-        self.assertEqual('new', p.new)  # Assert that the new of the post object is 'new'
 
     def test_json(self): # Test that the json method of the post object works as expected
-        p = Post('Test', 'Test Content', 'Author', 'new')  # Create a new post object
-        expected = {'title': 'Test', 'content': 'Test Content', 'author': 'Author', 'new': 'new'}
+        p = Post('Test', 'Test Content', 'Author')  # Create a new post object
+        expected = {'title': 'Test', 'content': 'Test Content', 'author': 'Author'}
         self.assertDictEqual(expected, p.json())
 
     def test_repr(self):
-        p = Post('Test', 'Test Content', 'Author', 'new')
+        p = Post('Test', 'Test Content', 'Author')
         self.assertEqual(str(p), 'Test, Author, Test Content')
+        self.assertEqual(p.__repr__(), 'Test, Author, Test Content')
+        self.assertEqual(p.__str__(), 'Test, Author, Test Content')
