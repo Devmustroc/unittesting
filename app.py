9b99b65d3284856aa5c3696c85812f7c8939a431
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-12 20:10:59 +0200

    Unittesting

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
 blogs = dict()  # blog_name : blog_object
 
 
@@ -7,6 +8,7 @@ def menu():
     # do something with that choice
     # eventually exit
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 
 def print_blogs():
